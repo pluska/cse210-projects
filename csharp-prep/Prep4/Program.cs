3d7653831b6285940aa123355139d2596ebf0a6f
Andres Pluska <pluskaandres@gmail.com>
2023-04-26 18:05:24 -0500

    Prep 4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Boolean mustContinue = true;
+        List<int> numbers = new List<int>();
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        } while (mustContinue);
+
+    int sum = 0;
+    int largest = 0;
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+            sum += number;
+        }
+
+        Console.WriteLine($"The number is: {sum}");
+        Console.WriteLine($"the average is: {sum/numbers.Count}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
