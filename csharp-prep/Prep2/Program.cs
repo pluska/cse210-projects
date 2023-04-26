76e5dc23e55db9e46234bf79e10329e76462d27c
Andres Pluska <pluskaandres@gmail.com>
2023-04-26 17:37:13 -0500

    Prep 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        char letter = 'F';
+
+        if (grade >= 90)
+        {
+            letter = 'A';
+        } else if (grade >=80)
+        {
+            letter = 'B';
+        } else if (grade >= 70)
+        {
+            letter = 'C';
+        } else if (grade >= 60)
+        {
+            letter = 'D';
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! you pass the class.");
+        } else
+        {
+            Console.WriteLine("Sorry you didn't pass the class.");
+        }
     }
 }
