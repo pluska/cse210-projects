cf897ae3d92327870c6e89a3670448e65c99bb1b
Andres Pluska <pluskaandres@gmail.com>
2023-04-26 17:49:22 -0500

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine("type a guess. ");
+        int guess_number = int.Parse(Console.ReadLine());
+
+        do
+        {
+            if (magic_number > guess_number)
+            {
+                Console.WriteLine("Lower");
+            } else if (magic_number < guess_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.WriteLine("type a guess. ");
+            guess_number = int.Parse(Console.ReadLine());
+        } while (magic_number != guess_number);
+
+            Console.WriteLine("You guess it!");
+
     }
 }
